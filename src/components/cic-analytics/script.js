20360ab602bd04d9d8c9628c058efbb9c4c0a88f
kashesandr <alexandresbox@gmail.com>
2015-08-02 17:03:27 +0300

    fix analytics, removed sharing, added contacts, a bit UX

--- src/components/cic-analytics/script.js
+++ src/components/cic-analytics/script.js
@@ -1,3 +1,14 @@
 Polymer({
-    is: "cic-analytics"
+    is: "cic-analytics",
+    ready: function(){
+
+        // Yandex.Metrika counter
+        (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter31513143 = new Ya.Metrika({ id:31513143, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");
+        // /Yandex.Metrika counter
+
+        // Google.Analytics counter
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-65325141-1', 'auto');ga('send', 'pageview');
+        // /Google.Analytics counter
+
+    }
 });
