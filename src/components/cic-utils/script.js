24fc971a62993df4a3dc466cfc4f6bc01dcefa42
Alexander Kasheverov <alexander.kasheverov@dataart.com>
2016-01-19 09:51:12 +0300

    Handle zero values #3

--- src/components/cic-utils/script.js
+++ src/components/cic-utils/script.js
@@ -61,5 +61,74 @@ Polymer({
 
     isDefined: function(value){
         return typeof value !== 'undefined';
+    },
+
+    calculate: function(contributors) {
+
+        var contributors = contributors.filter(function(item){
+            return this.isDefined(item.value);
+        }.bind(this));
+
+        var tmpArr = [],
+            totalValue = 0,
+            equalPayment = 0,
+            debtors= [], lenders= [],
+            lender = null, debtor = null,
+            values = [], delta = 0;
+
+        totalValue = contributors.reduce(function(p,c){return p+ c.value}, 0);
+        equalPayment = totalValue/contributors.length;
+        tmpArr = contributors.map(function(d){
+            d.value = equalPayment - d.value;
+            return d;
+        });
+
+        debtors = tmpArr.filter(function(item){return item.value > 0}); // all positive
+        lenders = tmpArr.filter(function(item){return item.value < 0}); // all negative
+        lender = lenders[0];
+        debtor = debtors[0];
+        values = [];
+        delta = 0;
+
+        while (lenders.length > 0 && debtors.length > 0) {
+            lender = lender || lenders[0];
+            debtor = debtor || debtors[0];
+            delta = debtor.value + lender.value;
+
+            if (delta < 0) {
+                values.push({
+                    from: debtor.name,
+                    to: lender.name,
+                    value: debtor.value
+                });
+                lender.value += debtor.value;
+                debtor.value = 0;
+            } else {
+                values.push({
+                    from: debtor.name,
+                    to: lender.name,
+                    value: -lender.value
+                });
+                debtor.value += lender.value;
+                lender.value = 0;
+            }
+
+            if (debtor.value === 0) {
+                debtors.splice(0, 1);
+                debtor = null;
+            }
+            if (lender.value === 0) {
+                lenders.splice(0, 1);
+                lender = null;
+            }
+
+            delta = 0;
+        }
+
+        return {
+            values: values,
+            totalValue: totalValue,
+            equalPayment: equalPayment
+        };
     }
 });
