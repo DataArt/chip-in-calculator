b09460ad1d5f6a1de90c25ca57960fede9484bb4
Daniil Yastremskiy <Catharsis@post.cz>
2016-10-22 18:54:41 +0100

    Add rounding options

--- src/components/cic-result/script.js
+++ src/components/cic-result/script.js
@@ -16,6 +16,12 @@ Polymer({
                 }
             },
             notify: true
+        },
+        rounding: {
+            type: Number,
+            value: -1
+            //Paper-dropdown got a bug: https://github.com/PolymerElements/paper-dropdown-menu/issues/197
+            //So if here will be a real value (0-3) dropdown will be broken like this: http://i.imgur.com/IRWBeZm.png (because of i18n loading)
         }
     },
     ready: function(){
@@ -32,11 +38,27 @@ Polymer({
 
         return name || i18nMsgBehavior.properties.i18n.value.to_somebody;
     },
-    _getValue: function(value, precision){
+    _getValue: function(value, rounding, precision){
+        if (rounding && rounding >= 0) {
+            return value ? this._round(value, rounding) : 0;
+        };
+
         if (!precision)
             precision = 2;
         return value ? parseFloat(value).toFixed(precision) : 0;
     },
+
+    _round: function(value, rounding){
+        switch (rounding) {
+            case 1:
+                return Math.round(value);
+            case 2:
+                return Math.floor(value);
+            case 3:
+                return Math.ceil(value);
+        };
+    },
+
     _onClick: function(){
         this.fire('ok-clicked');
     },
